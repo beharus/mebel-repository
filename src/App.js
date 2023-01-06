1275bede4e1ee60ce82539450440949c60edaa1f
Botirjon <botirjonumurzoqov02.01.1991@gmail.com>
2023-01-06 22:18:10 +0500

    fixed some bags

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 import "./App.css";
 import Navbar from "./components/navbar";
 import Main from "./components/main";
-=======
-
-import './App.css';
-import Footer from './components/footer/footer';
-import Navbar from './components/navbar/navbar';
->>>>>>> 923ece1ad39a76d1f24a41b6cb703ed5ed007a70
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navbar />
       <Main />
-=======
-      {/*<Navbar />*/}
       <Footer />
->>>>>>> 923ece1ad39a76d1f24a41b6cb703ed5ed007a70
     </div>
   );
 }
