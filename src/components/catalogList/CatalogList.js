53a750509037d7d3f3eef60370e000a8a8322b63
unknown <sd979141@gmail.com>
2023-01-18 17:12:49 +0900

    some

--- src/components/catalogList/CatalogList.js
+++ src/components/catalogList/CatalogList.js
@@ -4,11 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import CatalogItem from "../catalogItem";
 import { filterdCatalogList } from "../../redux/actions";
 import Button from "../button/Button";
+import Slider from "react-slick";
 
 function CatalogList() {
   const { catalog, catalogFilter, catalogActive, filteredCatalog, search } =
     useSelector((state) => state.catalog);
   const dispatch = useDispatch();
+
+  const settings = {
+    speed: 500,
+    infinite:false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
 
   useEffect(() => {
     if (catalog.length) {
@@ -28,6 +36,28 @@ function CatalogList() {
         {filteredCatalog[0].map((elem) => (
           <CatalogItem key={elem.id} {...elem} />
         ))}
+      </div>
+      <div className=" pager-item w-1/12 mt-[60px] text-center mx-auto">
+        <Slider {...settings}>
+          <div className=" font-light text-xl leading-[22px] cursor-pointer hover:text-[#343434] hover:font-medium duration-200 text-[#34343480]">
+            <span>1</span>
+          </div>
+          <div className=" font-light text-xl leading-[22px] cursor-pointer hover:text-[#343434] hover:font-medium duration-200 text-[#34343480]">
+            <span>2</span>
+          </div>
+          <div className=" font-light text-xl leading-[22px] cursor-pointer hover:text-[#343434] hover:font-medium duration-200 text-[#34343480]">
+            <span>3</span>
+          </div>
+          <div className=" font-light text-xl leading-[22px] cursor-pointer hover:text-[#343434] hover:font-medium duration-200 text-[#34343480]">
+            <span>4</span>
+          </div>
+          <div className=" font-light text-xl leading-[22px] cursor-pointer hover:text-[#343434] hover:font-medium duration-200 text-[#34343480]">
+            <span>5</span>
+          </div>
+          <div className=" font-light text-xl leading-[22px] cursor-pointer hover:text-[#343434] hover:font-medium duration-200 text-[#34343480]">
+            <span>6</span>
+          </div>
+        </Slider>
       </div>
       <div className="my-[80px] flex justify-center">
         <Button text="смотреть все" />
