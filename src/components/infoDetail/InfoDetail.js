f14d9292063f43ed5160e6b8d96dbb276ac6deb0
unknown <sd979141@gmail.com>
2023-01-18 18:09:14 +0900

    some

--- src/components/infoDetail/InfoDetail.js
+++ src/components/infoDetail/InfoDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Button from "../button/Button";
 
 function InfoDetail() {
   const { infoDetail } = useSelector((state) => state.catalog);
@@ -16,8 +17,21 @@ function InfoDetail() {
       </div>
       <div className="col-md-4">
         <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{description}</p>
+          <h5 className="card-title text-[35px] font-bold leading-[52px] font-raleway mb-[30px]">
+            {name}
+          </h5>
+          <h2 className=" font-raleway text-[18px] font-medium leading-[21px] capitalize text-[#343434] mb-[15px]">
+            Коллекция: {/*{Modern}*/}
+          </h2>
+          <p className="card-text text-[18px] font-raleway leading-[27px] font-light">
+            {description}
+          </p>
+          <p className=" text-[#ff9619] leading-[27px] text-xl font-semibold uppercase mt-[15px] mb-[30px]">
+            от {/*65 000*/} uah
+          </p>
+          <div className=" mb-[200px]">
+            <Button text="просчет стоимости" />
+          </div>
         </div>
       </div>
     </div>
